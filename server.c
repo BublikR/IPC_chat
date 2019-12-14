2070b925d52535db499aed5861360fea2a4c7eaa
Roman Bublik <sir.bublik@gmail.com>
2019-12-14 13:38:11 +0200

    Created multiconnection server

--- server.c
+++ server.c
@@ -18,7 +18,9 @@ int main(int argc, char **argv)
 {
     int port = 8080;
     char buf[BUF_LEN];
+    char name[80];
     struct sockaddr_in serverSockAddr;
+    struct LNode *client = NULL;
 
     signal(SIGINT, exit_funct); // Ctrl+C
 
@@ -28,7 +30,7 @@ int main(int argc, char **argv)
     // Created a linked list head. Save the sokets information in it.
     // Store the main server socket information in the linked list head.
     struct LNode* head = pushLNode(NULL, listenSock, "SERVER");
-        
+
     serverSockAddr.sin_family = AF_INET;
     serverSockAddr.sin_port = htons(port);
     serverSockAddr.sin_addr.s_addr = INADDR_ANY;
@@ -47,46 +49,70 @@ int main(int argc, char **argv)
         return 1;
     }
     printf("Server listen ...\n");
-    
-    struct sockaddr_in clientAddr;
-    socklen_t clientAddrLen = sizeof(clientAddr);
-
-    // Receiving a connection from a client
-    int clientSock = accept(listenSock, (struct sockaddr*) &clientAddr, &clientAddrLen);
-    if(clientSock == -1)
-    {
-        printf("Error accept\n");
-        return 1;
-    }
 
     // Communication with the client in the loop
     while(1)
     {
-        if(recv(clientSock, buf, sizeof(buf), 0) <= 0)
+        fd_set readfds;
+        int max_d = listenSock;
+
+        FD_ZERO(&readfds);
+        FD_SET(listenSock, &readfds);
+        
+        client = head;
+        while(client->next)
         {
-            printf("Error reading of client message\n");
-            clientSock = accept(listenSock, (struct sockaddr*) &clientAddr, &clientAddrLen);
+            client = client->next;
+            FD_SET(client->socket, &readfds);
+            if(client->socket > max_d)
+                max_d = client->socket;
+        }
+        
+        if(select(max_d+1, &readfds, NULL, NULL, NULL) < 1)
+        {
+            printf("Error select\n");
+            return 1;
+        }
+        
+        // Received a new connection request
+        if(FD_ISSET(listenSock, &readfds))
+        {
+            int clientSock = accept(listenSock, NULL, NULL);
             if(clientSock == -1)
             {
                 printf("Error accept\n");
                 return 1;
             }
-            if(recv(clientSock, buf, sizeof(buf), 0) <= 0)
-            {
-                printf("Error reading of client message\n");
-                return 1;
-            }
-        }
-
-        printf("Client say >> %s", buf);
-
-        if(send(clientSock, buf, sizeof(buf), 0) == -1)
-        {
-            printf("Error send message from client\n");
-            return 1;
+            // Save new client descriptor
+            sprintf(name, "%d", clientSock);
+            pushLNode(head, clientSock, name);
         }
         
+        client = head;
+        while(client->next)
+        {
+            client = client->next;
+            if(FD_ISSET(client->socket, &readfds))
+            {
+                if(recv(client->socket, buf, sizeof(buf), 0) <= 0)
+                {
+                    printf("Error reading of client %s message\n", client->name);
+                    return 1;
+                }
+                printf("Client %s say >> %s", client->name, buf);
+                
+                struct LNode *recipient = head;
+                while(recipient->next)
+                {
+                    recipient = recipient->next;
+                    if(send(recipient->socket, buf, sizeof(buf), 0) == -1)
+                    {
+                        printf("Error send message from client %s\n", recipient->name);
+                        return 1;
+                    }
+                }
+            }
+        }
     }
-
     return 0;
 }
